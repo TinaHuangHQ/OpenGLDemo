ba2821460fdc9a8ee09250dbbfc0f0b63014dcad
TinaHuangHQ <113064926@qq.com>
2020-08-26 15:36:37 +0800

    GLSL纹理翻转方案2：解压图片的时将图片源文件翻转

---
+++ 案例04 - GLSL纹理翻转/GLSL纹理翻转02-解压图片时，将图片源文件翻转/案例04 - GLSL纹理翻转02/案例04 - GLSL纹理翻转02/HView.m
@@ -0,0 +1,191 @@
+//
+//  HView.m
+//  案例04 - GLSL纹理翻转01
+//
+//  Created by macbook pro on 2020/8/26.
+//  Copyright © 2020 hq. All rights reserved.
+//
+
+#import <OpenGLES/ES2/gl.h>
+#import "HView.h"
+
+@interface HView ()
+
+//在iOS和tvOS上绘制OpenGL ES内容的图层，继承与CALayer
+@property(nonatomic,strong)CAEAGLLayer *myEagLayer;
+
+@property(nonatomic,strong)EAGLContext *myContext;
+
+@property(nonatomic,assign)GLuint myColorRenderBuffer;
+@property(nonatomic,assign)GLuint myColorFrameBuffer;
+
+@property(nonatomic,assign)GLuint myPrograme;
+
+@end
+
+@implementation HView
+
+- (void)layoutSubviews{
+    //1.设置图层
+    [self setupLayer];
+    //2.设置图形上下文
+    [self setupContext];
+    //3.清空缓存区
+    [self deleteRenderAndFrameBuffer];
+    //4.设置RenderBuffer
+    [self setupRenderBuffer];
+    //5.设置FrameBuffer
+    [self setupFrameBuffer];
+    //6.开始绘制
+    [self renderLayer];
+}
+
+-(void)setupLayer{
+    self.myEagLayer = (CAEAGLLayer*)self.layer;
+    [self setContentScaleFactor:[[UIScreen mainScreen] scale]];
+    self.myEagLayer.drawableProperties = @{
+        kEAGLDrawablePropertyRetainedBacking:@false,
+        kEAGLDrawablePropertyColorFormat:kEAGLColorFormatRGBA8
+    };
+}
+
++ (Class)layerClass{
+    return [CAEAGLLayer class];
+}
+
+-(void)setupContext{
+    self.myContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [EAGLContext setCurrentContext:self.myContext];
+}
+
+-(void)deleteRenderAndFrameBuffer{
+    glDeleteBuffers(1, &_myColorRenderBuffer);
+    self.myColorRenderBuffer = 0;
+    
+    glDeleteBuffers(1, &_myColorFrameBuffer);
+    self.myColorFrameBuffer = 0;
+}
+
+-(void)setupRenderBuffer{
+    glGenRenderbuffers(1, &_myColorRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, self.myColorRenderBuffer);
+    [self.myContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:self.myEagLayer];
+}
+
+-(void)setupFrameBuffer{
+    glGenFramebuffers(1, &_myColorFrameBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, self.myColorFrameBuffer);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, self.myColorRenderBuffer);
+}
+
+-(void)renderLayer{
+    glClearColor(0.3, 0.45, 0.5, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    CGFloat scale = [[UIScreen mainScreen] scale];
+    glViewport(self.frame.origin.x * scale, self.frame.origin.y * scale, self.frame.size.width * scale, self.frame.size.height * scale);
+    
+    NSString* vertFile = [[NSBundle mainBundle] pathForResource:@"shaderv" ofType:@"vsh"];
+    NSString* fragFile = [[NSBundle mainBundle] pathForResource:@"shaderf" ofType:@"fsh"];
+    
+    self.myPrograme = [self loadShaders:vertFile Withfrag:fragFile];
+    glLinkProgram(self.myPrograme);
+    GLint linkStatus;
+    glGetProgramiv(self.myPrograme, GL_LINK_STATUS, &linkStatus);
+    if(linkStatus == GL_FALSE){
+        GLchar message[512];
+        glGetProgramInfoLog(self.myPrograme, sizeof(message), 0, message);
+        NSString *messageString = [NSString stringWithUTF8String:message];
+        NSLog(@"Program Link Error:%@",messageString);
+        return ;
+    }
+    
+    glUseProgram(self.myPrograme);
+    
+    GLfloat attrArr[] ={
+        0.5f, -0.5f, -1.0f,     1.0f, 0.0f,
+        -0.5f, 0.5f, -1.0f,     0.0f, 1.0f,
+        -0.5f, -0.5f, -1.0f,    0.0f, 0.0f,
+        
+        0.5f, 0.5f, -1.0f,      1.0f, 1.0f,
+        -0.5f, 0.5f, -1.0f,     0.0f, 1.0f,
+        0.5f, -0.5f, -1.0f,     1.0f, 0.0f,
+    };
+    
+    GLuint attrBuffer;
+    glGenBuffers(1, &attrBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, attrBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(attrArr), attrArr, GL_DYNAMIC_DRAW);
+    
+    GLuint position = glGetAttribLocation(self.myPrograme, "position");
+    glEnableVertexAttribArray(position);
+    glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), NULL);
+    
+    GLuint textCoor = glGetAttribLocation(self.myPrograme, "textCoordinate");
+    glEnableVertexAttribArray(textCoor);
+    glVertexAttribPointer(textCoor, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (GLfloat*)NULL+3);
+    
+    [self setupTexture:@"nn.jpg"];
+    
+    glUniform1f(glGetUniformLocation(self.myPrograme, "colorMap"), 0);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    
+    [self.myContext presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+-(GLuint)loadShaders:(NSString *)vert Withfrag:(NSString *)frag{
+    GLuint verShader, fragShader;
+    
+    GLuint program = glCreateProgram();
+    [self compileShader:&verShader type:GL_VERTEX_SHADER file:vert];
+    [self compileShader:&fragShader type:GL_FRAGMENT_SHADER file:frag];
+    
+    glAttachShader(program, verShader);
+    glAttachShader(program, fragShader);
+    
+    glDeleteShader(verShader);
+    glDeleteShader(fragShader);
+    
+    return program;
+}
+
+- (void)compileShader:(GLuint *)shader type:(GLenum)type file:(NSString *)file{
+    NSString* content = [NSString stringWithContentsOfFile:file encoding:NSUTF8StringEncoding error:nil];
+    const GLchar* source = (GLchar*)content.UTF8String;
+    
+    *shader = glCreateShader(type);
+    glShaderSource(*shader, 1, &source, NULL);
+    glCompileShader(*shader);
+}
+
+- (void)setupTexture:(NSString *)fileName{
+    CGImageRef image = [[UIImage imageNamed:fileName] CGImage];
+    
+    size_t width = CGImageGetWidth(image);
+    size_t height = CGImageGetHeight(image);
+    
+    GLubyte* imageData = (GLubyte*)calloc(width*height*4, sizeof(GLubyte));
+    
+    CGContextRef context = CGBitmapContextCreate(imageData, width, height, 8, width*4, CGImageGetColorSpace(image), kCGImageAlphaPremultipliedLast);
+    
+    CGRect rect = CGRectMake(0, 0, width, height);
+    
+    CGContextTranslateCTM(context, 0, height);
+    CGContextScaleCTM(context, 1, -1);
+    CGContextDrawImage(context, rect, image);
+    
+    CGContextRelease(context);
+    
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, (float)width, (float)height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData);
+    
+    free(imageData);
+}
+
+@end
---
+++ 案例04 - GLSL纹理翻转/GLSL纹理翻转02-解压图片时，将图片源文件翻转/案例04 - GLSL纹理翻转02/案例04 - GLSL纹理翻转02/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  案例04 - GLSL纹理翻转02
+//
+//  Created by macbook pro on 2020/8/26.
+//  Copyright © 2020 hq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
