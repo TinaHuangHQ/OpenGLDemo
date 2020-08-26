b0b7b35826e203bbaf1963eb233066f0192cc563
TinaHuangHQ <113064926@qq.com>
2020-08-26 15:01:17 +0800

    GLSL纹理翻转方案1：使用矩阵进行纹理顶点坐标翻转

---
+++ 案例04 - GLSL纹理翻转/GLSL纹理翻转01-旋转矩阵翻转图形，不翻转纹理/案例04 - GLSL纹理翻转01/案例04 - GLSL纹理翻转01/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  案例04 - GLSL纹理翻转01
+//
+//  Created by macbook pro on 2020/8/26.
+//  Copyright © 2020 hq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
