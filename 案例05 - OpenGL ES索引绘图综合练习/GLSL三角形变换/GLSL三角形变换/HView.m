c5d0ff9d7b7b19f3354b6fc673fa5cad731d1544
TinaHuangHQ <113064926@qq.com>
2020-08-31 15:20:26 +0800

    GLSL三角形变换，将某些代码抽成独立函数

--- 案例05 - OpenGL ES索引绘图综合练习/GLSL三角形变换/GLSL三角形变换/HView.m
+++ 案例05 - OpenGL ES索引绘图综合练习/GLSL三角形变换/GLSL三角形变换/HView.m
@@ -41,6 +41,10 @@
     [self deletBuffer];
     [self setupRenderBuffer];
     [self setupFrameBuffer];
+    [self setupProgram];
+    [self setupVertexData];
+    [self setupProjectionData];
+    [self setupModelViewData];
     [self render];
 }
 
@@ -83,20 +87,16 @@
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, self.myColorRenderBuffer);
 }
 
--(void)render{
-    glClearColor(0, 0.0, 0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    
-    GLfloat scale = [[UIScreen mainScreen] scale];
-    glViewport(self.frame.origin.x * scale, self.frame.origin.y * scale, self.frame.size.width * scale, self.frame.size.height * scale);
-    
+-(void)setupProgram{
     NSString* vertFile = [[NSBundle mainBundle] pathForResource:@"shaderv" ofType:@"vsh"];
     NSString* fragFile = [[NSBundle mainBundle] pathForResource:@"shaderf" ofType:@"fsh"];
     
     self.myProgram = [self loadShader:vertFile frag:fragFile];
     glLinkProgram(self.myProgram);
     glUseProgram(self.myProgram);
-    
+}
+
+-(void)setupVertexData{
     //(1)顶点数组 前3顶点值（x,y,z），后3位颜色值(RGB)
     GLfloat attrArr[] = {
         -0.5f, 0.5f, 0.0f,      1.0f, 0.0f, 1.0f, //左上0
@@ -105,16 +105,6 @@
         
         0.5f, -0.5f, 0.0f,      1.0f, 1.0f, 1.0f, //右下3
         0.0f, 0.0f, 1.0f,       0.0f, 1.0f, 0.0f, //顶点4
-    };
-    
-    //(2).索引数组
-    GLuint indices[] = {
-        0, 3, 2,
-        0, 1, 3,
-        0, 2, 4,
-        0, 4, 1,
-        2, 3, 4,
-        1, 4, 3,
     };
     
     GLuint bufferID;
@@ -129,34 +119,45 @@
     GLuint positionColor = glGetAttribLocation(self.myProgram, "positionColor");
     glEnableVertexAttribArray(positionColor);
     glVertexAttribPointer(positionColor, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (GLfloat*)NULL+3);
-    
+}
+
+-(void)setupProjectionData{
     GLuint projectionMatrixSlot = glGetUniformLocation(self.myProgram, "projectionMatrix");
-    GLuint modelViewMatrixSlot = glGetUniformLocation(self.myProgram, "modelViewMatrix");
-    
-    float width = self.frame.size.width;
-    float height = self.frame.size.height;
-    
     KSMatrix4 _projectionMatrix;
     ksMatrixLoadIdentity(&_projectionMatrix);
-    float aspect = width/height;
+    float aspect = self.frame.size.width/self.frame.size.height;
     
     ksPerspective(&_projectionMatrix, 30, aspect, 5.0, 20.0);
+    ksTranslate(&_projectionMatrix, 0, 0, -10.0);
     glUniformMatrix4fv(projectionMatrixSlot, 1, GL_FALSE, &_projectionMatrix.m[0][0]);
-    
+}
+
+- (void)setupModelViewData{
+    GLuint modelViewMatrixSlot = glGetUniformLocation(self.myProgram, "modelViewMatrix");
+        
     KSMatrix4 _modelViewMatrix;
     ksMatrixLoadIdentity(&_modelViewMatrix);
-    ksTranslate(&_modelViewMatrix, 0, 0, -10.0);
-    
-    KSMatrix4 _rotationMatrix;
-    ksMatrixLoadIdentity(&_rotationMatrix);
-    ksRotate(&_rotationMatrix, xDegree, 1, 0, 0);
-    ksRotate(&_rotationMatrix, yDegree, 0, 1, 0);
-    ksRotate(&_rotationMatrix, zDegree, 0, 0, 1);
-    
-    ksMatrixMultiply(&_modelViewMatrix, &_rotationMatrix, &_modelViewMatrix);
     glUniformMatrix4fv(modelViewMatrixSlot, 1, GL_FALSE, &_modelViewMatrix.m[0][0]);
+}
+
+-(void)render{
+    glClearColor(0, 0.0, 0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    GLfloat scale = [[UIScreen mainScreen] scale];
+    glViewport(self.frame.origin.x * scale, self.frame.origin.y * scale, self.frame.size.width * scale, self.frame.size.height * scale);
     
     glEnable(GL_CULL_FACE);
+    
+    //(2).索引数组
+    GLuint indices[] = {
+        0, 3, 2,
+        0, 1, 3,
+        0, 2, 4,
+        0, 4, 1,
+        2, 3, 4,
+        1, 4, 3,
+    };
     
     glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(indices[0]), GL_UNSIGNED_INT, indices);
     
@@ -178,7 +179,6 @@
     
     return program;
 }
-
 
 -(void)compileShader:(GLuint *)shader type:(GLenum)type file:(NSString *)file{
     NSString* content = [NSString stringWithContentsOfFile:file encoding:NSUTF8StringEncoding error:nil];
@@ -216,6 +216,21 @@
     xDegree += bX*5;
     yDegree += bY*5;
     zDegree += bZ*5;
+    
+    GLuint modelViewMatrixSlot = glGetUniformLocation(self.myProgram, "modelViewMatrix");
+    
+    KSMatrix4 _modelViewMatrix;
+    ksMatrixLoadIdentity(&_modelViewMatrix);
+    
+    KSMatrix4 _rotationMatrix;
+    ksMatrixLoadIdentity(&_rotationMatrix);
+    ksRotate(&_rotationMatrix, xDegree, 1, 0, 0);
+    ksRotate(&_rotationMatrix, yDegree, 0, 1, 0);
+    ksRotate(&_rotationMatrix, zDegree, 0, 0, 1);
+    
+    ksMatrixMultiply(&_modelViewMatrix, &_rotationMatrix, &_modelViewMatrix);
+    glUniformMatrix4fv(modelViewMatrixSlot, 1, GL_FALSE, &_modelViewMatrix.m[0][0]);
+    
     [self render];
 }
 
