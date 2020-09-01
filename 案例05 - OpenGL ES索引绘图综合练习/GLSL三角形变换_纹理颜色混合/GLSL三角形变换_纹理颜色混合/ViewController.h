52c90cd82dfc9400f6b9df48e0360d3678406da7
TinaHuangHQ <113064926@qq.com>
2020-09-01 11:38:32 +0800

    GLSL三角形变换，增加纹理颜色混合项目名称修改

---
+++ 案例05 - OpenGL ES索引绘图综合练习/GLSL三角形变换_纹理颜色混合/GLSL三角形变换_纹理颜色混合/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GLSL三角形变换_纹理颜色混合
+//
+//  Created by macbook pro on 2020/9/1.
+//  Copyright © 2020 hq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 案例05 - OpenGL ES索引绘图综合练习/GLSL三角形变换_纹理颜色混合/GLSL三角形变换_纹理颜色混合/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GLSL三角形变换_纹理颜色混合
+//
+//  Created by macbook pro on 2020/9/1.
+//  Copyright © 2020 hq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
